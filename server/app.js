7f1dbed8075a2590d0d8fa99aa55aec8348a4716
Jim <jim@advancebass.com>
2020-11-19 11:57:42 -0800

    add new route for /about

--- server/app.js
+++ server/app.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Welcome to my API</h1>');
 });
 
+app.get('/about', (req, res) => {
+  res.send('<h1>Not much here yet</h1>');
+});
+
 app.listen(8080, console.log('http://localhost:8080'));
