517e0c802152a779cda6ca95feeb6660a27bd49e
Jim <jim@advancebass.com>
2020-11-19 12:25:13 -0800

    add contact route

--- server/app.js
+++ server/app.js
@@ -9,4 +9,8 @@ app.get('/about', (req, res) => {
   res.send('<h1>Not much here ye!</h1>');
 });
 
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact Page</h1>');
+});
+
 app.listen(8080, console.log('http://localhost:8080'));
