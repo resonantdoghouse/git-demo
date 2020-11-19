76f8ff04d4f7d5a644ee40ae5407bbbfc1cd01a0
Jim <jim@advancebass.com>
2020-11-19 11:51:52 -0800

    add express and home endpoiint & package.json

---
+++ server/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to my API</h1>');
+});
+
+app.listen(8080, console.log('http://localhost:8080'));
