777046b75e716ecd5691361054ed00f5179c7e33
Jim <jim@advancebass.com>
2020-11-19 12:13:39 -0800

    attempt to make merge conflict

--- server/app.js
+++ server/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome to my API</h1>');
+  res.send('<h1>Welcome to my AP1</h1>');
 });
 
 app.get('/about', (req, res) => {
-  res.send('<h1>Not much here yet</h1>');
+  res.send('<h1>Not much here ye!</h1>');
 });
 
 app.listen(8080, console.log('http://localhost:8080'));
